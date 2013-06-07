9dca3ea4c04898344f52dda1f7b313cb88412991
Fernando Alonso <krakatoa1987@gmail.com>
2013-06-06 23:28:58 -0300

    Removed commented lines

--- 01.1_req_rep/req.js
+++ 01.1_req_rep/req.js
@@ -2,13 +2,6 @@ var zmq = require("zmq");
 
 var request = zmq.socket("req");
 
-//request.linger = 1000;
-//request.reconnect_ivl = 1000;
-//request.setsockopt(zmq.ZMQ_LINGER, 1000);
-//request.setsockopt(zmq.ZMQ_RECONNECT_IVL_MAX, 1000);
-
-//request.setsockopt(zmq.ZMQ_SNDHWM, 1);
-
 request.connect("tcp://127.0.0.1:5001");
 request.connect("tcp://127.0.0.1:5002");
 request.connect("tcp://127.0.0.1:5003");
@@ -18,13 +11,9 @@ count = 0;
 
 setInterval(function() {
   var t = request.send("msg nro. " + count++ + " (from " + id + ")");
-  //console.log(t);
-}, 10);
-
-request.on("error", function(err) {
-  console.log(err);
-});
+  console.log(t);
+}, 1000);
 
 request.on("message", function(msg) {
-  //console.log("Response from: " + msg.toString());
+  console.log("Response from: " + msg.toString());
 });
